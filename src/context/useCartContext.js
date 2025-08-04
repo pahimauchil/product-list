2eaa1506873df5a65fb2bd1057441fbec4f9f3c4
Pahima R Uchil <uchilpahi@gmail.com>
2025-08-05 01:08:51 +0530

    Fix cartContext import in useCartContext

--- src/context/useCartContext.js
+++ src/context/useCartContext.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { cartContext } from "./CartContext";
+import { cartContext } from "./cartContext";
 
 export const useCartContext = () => {
     const context = useContext(cartContext);
